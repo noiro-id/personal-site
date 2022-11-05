3b1071dc7898a0fcb361fe31bd48373cb227052f
Kusuma Lingga <25038088+noiro-id@users.noreply.github.com>
2022-11-05 15:49:18 +0700

    slight adjustment on screen positioning.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div class="d-flex justify-content-md-center align-items-center vh-100">
+      <div class="d-flex justify-content-center align-items-center vh-100">
         <div>Under Maintenance</div>
       </div>
     </div>
